cbda2523f26dba32b97375f5ffcf55d77db17479
Reinis Ikass <reinis.ikass@gmail.com>
2022-03-18 14:36:11 +0200

    add sitemap generator

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
 const { DateTime } = require("luxon");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 
 module.exports = (config) => {
   config.addPassthroughCopy({ 'public': './' });
   // Copy `img/` to `_site/img`
   config.addPassthroughCopy("src/img");
   config.addPlugin(embedYouTube);
+  config.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://www.walkerteareview.com/",
+    },
+  });
   config.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
